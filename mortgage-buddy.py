57fc603c30aaa5871d62011ee48b01714442b7f6
Eric Lee <eric.hyunil.lee@gmail.com>
2024-01-02 13:55:24 -0800

    Added basic python file for mortgage calculator and intro

---
+++ mortgage-buddy.py
@@ -0,0 +1,42 @@
+import random
+
+print("""
+
+888b     d888                  888                                              888888b.                 888      888          
+8888b   d8888                  888                                              888  "88b                888      888          
+88888b.d88888                  888                                              888  .88P                888      888          
+888Y88888P888  .d88b.  888d888 888888  .d88b.   8888b.   .d88b.   .d88b.        8888888K.  888  888  .d88888  .d88888 888  888 
+888 Y888P 888 d88""88b 888P"   888    d88P"88b     "88b d88P"88b d8P  Y8b       888  "Y88b 888  888 d88" 888 d88" 888 888  888 
+888  Y8P  888 888  888 888     888    888  888 .d888888 888  888 88888888       888    888 888  888 888  888 888  888 888  888 
+888   "   888 Y88..88P 888     Y88b.  Y88b 888 888  888 Y88b 888 Y8b.           888   d88P Y88b 888 Y88b 888 Y88b 888 Y88b 888 
+888       888  "Y88P"  888      "Y888  "Y88888 "Y888888  "Y88888  "Y8888        8888888P"   "Y88888  "Y88888  "Y88888  "Y88888 
+                                           888               888                                                           888 
+                                      Y8b d88P          Y8b d88P                                                      Y8b d88P 
+                                       "Y88P"            "Y88P"                                                        "Y88P"  
+
+""")
+
+
+
+print("""Hello, please select one of the options: 
+      
+      1. Calculate mortgage payment
+
+      2. See if you qualify for a mortgage
+
+      3. Current average interest rate
+
+      4. Helpful tips about mortgage
+
+      """)
+
+name = input('Enter your choice: ')
+
+# print("you've selected " + name)
+
+if name == '4':
+    print("Mortgage lending is not fun")
+
+def mortgage_calculator(loan_amount, interest_rate, amortization_term)
+    
+def loan_qualification(income, liabilities)
